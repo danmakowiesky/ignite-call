d700f8b22dd542abafc90390cf5d3448660243be
danmakowiesky <dvm1993@hotmail.com>
2023-02-19 09:45:04 -0300

    validação com zod

--- src/pages/home/components/ClaimUserNameForm/index.tsx
+++ src/pages/home/components/ClaimUserNameForm/index.tsx
@@ -1,15 +1,48 @@
-import { Button, TextInput } from '@ignite-ui/react'
+import { Button, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
-import { Form } from './styles'
+import { useForm } from 'react-hook-form'
+import {
+  ClaimUserNameFormData,
+  ClaimUserNameFormSchema,
+} from '@/pages/home/utils/validate'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Form, FormAnnotation } from './styles'
 
 export function ClaimUserNameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUserNameFormData>({
+    resolver: zodResolver(ClaimUserNameFormSchema),
+  })
+
+  async function handleClaimUserFormData(data: ClaimUserNameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form>
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
-      <Button size="sm" type="submit">
-        Reservar
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUserFormData)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuário"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username?.message
+            : 'Digite o nome do usuário Desejado'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
---
+++ src/pages/home/utils/validate.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+
+export const ClaimUserNameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
+    })
+    .transform((username) => username.toLocaleLowerCase()),
+})
+
+export type ClaimUserNameFormData = z.infer<typeof ClaimUserNameFormSchema>
